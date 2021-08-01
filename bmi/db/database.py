c34a0accc45c1b0c0c77465b61aae1b5e7625bf3
Shantanu-techprescient <87164229+Shantanu-techprescient@users.noreply.github.com>
2021-08-01 13:31:01 +0530

    Update database.py

--- bmi/db/database.py
+++ bmi/db/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./BmiData.db" # determine read file paths  in python
+SQLALCHEMY_DATABASE_URL = "sqlite:///bmi/BmiData.db" # determine read file paths  in python
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": True} #most imp
